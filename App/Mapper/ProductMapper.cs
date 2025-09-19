9fa1c7464d719249a852e417a664c0d831822786
Rselmane <rayan.selmane@etu.univ-smb.fr>
2025-09-19 16:48:30 +0200

    début DTO

---
+++ App/Mapper/ProductMapper.cs
@@ -0,0 +1,20 @@
+﻿using App.DTO;
+using App.Models;
+using AutoMapper;
+
+namespace App.Mapper
+{
+    public class ProductMapper : Profile
+    {
+        public ProductMapper() 
+        {
+            
+            // Mapping Produit -> ProduitDto
+            CreateMap<Produit, ProduitDto>()
+                .ForMember(dest => dest.Marque,
+                           opt => opt.MapFrom(src => src.MarqueNavigation != null ? src.MarqueNavigation.NomMarque : null))
+                .ForMember(dest => dest.Type,
+                           opt => opt.MapFrom(src => src.TypeProduitNavigation != null ? src.TypeProduitNavigation.NomTypeProduit : null));
+        }
+    }
+}
