b1a2fb28ca4d895763a86de1c24f70c1d4e7310e
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-09-28 23:19:10 +0200

    Ajout du BrandDTO et du BrandController

---
+++ App/Controllers/BrandController.cs
@@ -0,0 +1,71 @@
+﻿using App.DTO;
+using App.Models;
+using App.Models.EntityFramework;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers;
+
+
+[Route("api/brands")]
+[ApiController]
+public class BrandController(
+    IMapper mapper,
+    IDataRepository<Brand> manager,
+    AppDbContext context
+    ) : ControllerBase
+{
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<BrandDTO?>> Get(int id)
+    {
+        var result = await manager.GetByIdAsync(id);
+        return result == null ? NotFound() : mapper.Map<BrandDTO>(result);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        ActionResult<Brand?> brand = await manager.GetByIdAsync(id);
+        if (brand.Value == null)
+            return NotFound();
+        await manager.DeleteAsync(brand.Value);
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<BrandDTO>>> GetAll()
+    {
+        var brands = await manager.GetAllAsync();
+        var brandDTOs = mapper.Map<IEnumerable<BrandDTO>>(brands);
+        return new ActionResult<IEnumerable<BrandDTO>>(brandDTOs);
+    }
+
+    // TODO Create BrandController
+    //
+    //
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(int id, [FromBody] Brand brand)
+    {
+        if (id != brand.IdBrand)
+        {
+            return BadRequest();
+        }
+        ActionResult<Brand?> brandToUpdate = await manager.GetByIdAsync(id);
+        if (brandToUpdate.Value == null)
+        {
+            return NotFound();
+        }
+        await manager.UpdateAsync(brandToUpdate.Value, brand);
+        return NoContent();
+    }
+}
---
+++ App/DTO/BrandDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace App.DTO
+{
+    public class BrandDTO
+    {
+        public int Id { get; set; }
+
+        public string? Nom { get; set; }
+    }
+}
