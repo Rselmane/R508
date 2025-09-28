5c5f299a2fcedbf26d45582e1ab7064a04a30f93
Luzagar <luzagar.abonner@gmail.com>
2025-09-28 23:31:53 +0200

    BrandController OK

--- App/Controllers/BrandController.cs
+++ App/Controllers/BrandController.cs
@@ -46,9 +46,27 @@ public class BrandController(
         return new ActionResult<IEnumerable<BrandDTO>>(brandDTOs);
     }
 
-    // TODO Create BrandController
-    //
-    //
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ProductDetailDTO>> Create([FromBody] BrandDTO dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        // Mapping DTO → Produit
+        Brand brand = mapper.Map<Brand>(dto);
+
+
+        // Sauvegarde du produit
+        await manager.AddAsync(brand);
+
+        // Retourner le détail du produit créé
+        ProductDetailDTO BrandDetail = mapper.Map<ProductDetailDTO>(brand);
+        return CreatedAtAction("Get", new { id = brand.IdBrand }, BrandDetail);
+    }
 
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
