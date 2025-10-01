e1064351897f5707c53e423a466b26310f445541
Robin MILLET <millerob@etu.univ-smb.fr>
2025-10-01 15:43:50 +0200

    Base pour les tests E2E, utilisant la librairie Playwright Pour l'instant ne test que l'existence de la page principale en attendant le reste du Blazor API pas testée non plus en E2E, à voir Si vous ne parvenez pas à run les tests E2E, c'est probablement normal ; il vous faut dotnet-playwright et installer un navigateur chromium dans votre HOME > dotnet playwright install chromium

---
+++ Tests/EndToEnd/BaseE2E.cs
@@ -0,0 +1,54 @@
+﻿using App.Controllers;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Tests.EndToEnd;
+
+[TestClass]
+[TestCategory("e2e")]
+public class BaseE2E
+{
+    private Helper _helper;
+    private IPlaywright _playwright;
+    private IBrowser _browser;
+    private IPage _page;
+
+    [TestInitialize]
+    public async Task InitializeAsync()
+    {
+        _helper = new Helper();
+        await _helper.InitializeAsync();
+        await Task.Delay(10000);
+        _playwright = await Playwright.CreateAsync();
+        _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true
+        });
+        _page = await _browser.NewPageAsync();
+        await _page.GotoAsync("http://localhost:5109");
+    }
+
+    [TestMethod]
+    public async Task ShouldLoadHomePage()
+    {
+        // When : J'ouvre la page d'accueil
+        var title = await _page.TitleAsync();
+        // Then : Le titre de la page est correct
+        Assert.AreEqual("Home", title);
+    }
+
+    [TestCleanup]
+    public async Task DisposeAsync()
+    {
+        await _browser.DisposeAsync();
+        _playwright.Dispose();
+        await _helper.DisposeAsync();
+    }
+
+}
---
+++ Tests/EndToEnd/HelperE2E.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Kralizek.AutoFixture.Extensions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests.EndToEnd;
+
+public class Helper
+{
+    private Process _blazor;
+
+    async public Task InitializeAsync()
+    { 
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string projectDirectory = Directory.GetParent(currentDirectory).Parent.Parent.Parent.FullName;
+
+        var factory = new WebApplicationFactory<App.Program>();
+        var client = factory.CreateClient();
+
+        _blazor = Process.Start(new ProcessStartInfo {
+            FileName = "dotnet",
+            Arguments = "run --project BlazorApp/BlazorApp.csproj --launch-profile http --urls \"http://localhost:5109\"",
+            WorkingDirectory = projectDirectory,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        });
+
+        _blazor.OutputDataReceived += (s, e) => Console.WriteLine($"[BLZ] {e.Data}");
+        _blazor.ErrorDataReceived += (s, e) => Console.WriteLine($"[BLZ-ERR] {e.Data}");
+        _blazor.Start();
+        _blazor.BeginOutputReadLine();
+        _blazor.BeginErrorReadLine();
+
+        var tcs = new TaskCompletionSource();
+        _blazor.OutputDataReceived += (s, e) =>
+        {
+            if (e.Data != null && e.Data.Contains("Now listening on"))
+                tcs.TrySetResult();
+        };
+        await Task.WhenAny(tcs.Task, Task.Delay(10000));
+    }
+
+    public Task DisposeAsync()
+    {
+        if (!_blazor.HasExited) _blazor.Kill();
+        return Task.CompletedTask;
+    }
+}
