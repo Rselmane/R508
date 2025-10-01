7483dbbffc8d2fef25bf19e98a717ee96b8d8ca7
Justin Bonansea <Justin.Bonansea@etu.univ-smb.fr>
2025-10-01 15:17:41 +0200

    Ajout des tests unitaires/intégrations pour TypeProduct et modification des noms de variables pour BrandControllerTest

--- Tests/Controllers/TypeProductControllerTest.cs
+++ Tests/Controllers/TypeProductControllerTest.cs
@@ -14,55 +14,173 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tests.AutoMapper;
 
-namespace Tests.Controllers
+namespace Tests.Controllers;
+
+[TestClass]
+[TestSubject(typeof(TypeProductController))]
+[TestCategory("integration")]
+public class TypeProductControllerTest : AutoMapperConfigTests
 {
+    private AppDbContext _context;
+    private TypeProductController _typeProductdController;
+    private IDataRepository<TypeProduct> _manager;
 
-    [TestClass]
-    [TestSubject(typeof(TypeProductController))]
-    [TestCategory("integration")]
-    public class TypeProductControllerTest
+    // Objets communs pour les tests
+    private TypeProduct _typeProdcutKeybord;
+    private TypeProduct _typeProdcutScreen;
+    private TypeProduct _typeProdcutMouseEntity;
+    private TypeProductDTO _typeProductDtoMouse;
+
+    [TestInitialize]
+    public void Initialize()
     {
-        private readonly AppDbContext _context;
-        private readonly TypeProductController _typeProductController;
-        private readonly MapperConfiguration _config;
+        // Contexte et mapper
+        _context = new AppDbContext();
 
-        public TypeProductControllerTest()
-        {
-            _context = new AppDbContext();
+        // Manager et controller
+        _manager = new TypeProductManager(_context);
+        _typeProductdController = new TypeProductController(_mapper, _manager, _context);
 
-            _config = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<TypeProductMapper>();
-            }, new LoggerFactory());
+        // Données communes : entities
+        _typeProdcutKeybord = new TypeProduct { TypeProductName = "Adidas" };
+        _typeProdcutScreen = new TypeProduct { TypeProductName = "Nike" };
 
-            _config.AssertConfigurationIsValid();
-            IMapper mapper = _config.CreateMapper();
+        // DTO
+        _typeProductDtoMouse = new TypeProductDTO { Name = "Corsair" };
 
-            IDataRepository<TypeProduct> manager = new TypeProductManager(_context);
-            _typeProductController = new TypeProductController(mapper, manager, _context);
-        }
+        // Mapper DTO → Entity et ajouter en DB
+        _typeProdcutMouseEntity = _mapper.Map<TypeProduct>(_typeProductDtoMouse);
 
-        [TestMethod]
-        public void ShouldGetTypeProduct()
-        {
-            // Given : un produit en base de donnée
-            TypeProduct typeProductInDb = new TypeProduct()
-            {
-                TypeProductName = "School"
-            };
-            _context.TypeProducts.Add(typeProductInDb);
-            _context.SaveChanges();
+        // Ajout initial en DB
+        _context.TypeProducts.AddRange(_typeProdcutKeybord, _typeProdcutScreen, _typeProdcutMouseEntity);
+        _context.SaveChanges();
+    }
 
-            // When : J'appelle la méthode get de mon api pour récupérer le produit
-            ActionResult<TypeProductDTO> action = _typeProductController.Get(typeProductInDb.IdTypeProduct).GetAwaiter().GetResult();
-            TypeProductDTO returnTypeProduct = action.Value;
+    [TestMethod]
+    public void ShouldGetBrand()
+    {
+        // When : J'appelle la méthode get de mon api pour récupérer le produit
+        ActionResult<TypeProductDTO> action = _typeProductdController.Get(_typeProdcutKeybord.IdTypeProduct).GetAwaiter().GetResult();
+        TypeProductDTO returnTypeProduct = action.Value;
 
-            // Then : On récupère le produit et le code de retour est 200
-            Assert.IsNotNull(action);
-            Assert.IsInstanceOfType(action.Value, typeof(TypeProductDTO));
-            Assert.AreEqual(typeProductInDb.TypeProductName, returnTypeProduct.Name);
-        }
+        // Then : On récupère le produit et le code de retour est 200
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action.Value, typeof(TypeProductDTO));
+        Assert.AreEqual(_typeProdcutKeybord.TypeProductName, returnTypeProduct.Name);
+    }
 
+    [TestMethod]
+    public void ShouldDeleteBrand()
+    {
+        // When : Je souhaite supprimé un produit depuis l'API
+        IActionResult action = _typeProductdController.Delete(_typeProdcutKeybord.IdTypeProduct).GetAwaiter().GetResult();
+
+        // Then : Le produit a bien été supprimé et le code HTTP est NO_CONTENT (204)
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action, typeof(NoContentResult));
+        Assert.IsNull(_context.Products.Find(_typeProdcutKeybord.IdTypeProduct));
+    }
+
+    [TestMethod]
+    public void ShouldNotDeleteBrandBecauseBrandDoesNotExist()
+    {
+        // When : Je souhaite supprimé un produit depuis l'API
+        IActionResult action = _typeProductdController.Delete(_typeProdcutKeybord.IdTypeProduct).GetAwaiter().GetResult();
+
+        // Then : Le produit a bien été supprimé et le code HTTP est NO_CONTENT (204)
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action, typeof(NotFoundResult));
+    }
+
+    [TestMethod]
+    public void ShouldGetAllBrands()
+    {
+        // When : On souhaite récupérer tous les produits
+        var typeProducts = _typeProductdController.GetAll().GetAwaiter().GetResult();
+
+        // Then : Tous les produits sont récupérés
+        Assert.IsNotNull(typeProducts);
+        Assert.IsInstanceOfType(typeProducts.Value, typeof(IEnumerable<TypeProduct>));
+    }
+
+    [TestMethod]
+    public void GetBrandShouldReturnNotFound()
+    {
+        // When : J'appelle la méthode get de mon api pour récupérer le produit
+        ActionResult<TypeProductDTO> action = _typeProductdController.Get(0).GetAwaiter().GetResult(); ;
+
+        // Then : On ne renvoie rien et on renvoie 404
+        Assert.IsInstanceOfType(action.Result, typeof(NotFoundResult), "Ne renvoie pas 404");
+        Assert.IsNull(action.Value, "Le produit n'est pas null");
+    }
+
+    [TestMethod]
+    public void ShouldCreateBrand()
+    {
+        // When
+        ActionResult<TypeProductDTO> action = _typeProductdController.Create(_typeProductDtoMouse).GetAwaiter().GetResult(); // ✅ Corrigé le type
+
+        // Then
+        var createdResult = (CreatedAtActionResult)action.Result;
+        var createdDto = (TypeProductDTO)createdResult.Value;
+
+        TypeProduct typeProductInDb = _context.TypeProducts.Find(createdDto.Name);  // pas sûr de ça 
+
+        Assert.IsNotNull(typeProductInDb);
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action.Result, typeof(CreatedAtActionResult));
+        Assert.AreEqual(_typeProductDtoMouse.Name, typeProductInDb.TypeProductName);
+    }
+
+    [TestMethod]
+    public void ShouldUpdateBrand()
+    {
+        // Given
+        _typeProdcutKeybord.TypeProductName = "KeybordV2";
+
+        // When
+        IActionResult action = _typeProductdController.Update(_typeProdcutKeybord.IdTypeProduct, _typeProdcutKeybord).GetAwaiter().GetResult();
+
+        // Then
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action, typeof(NoContentResult));
+
+        TypeProduct editedTypeProductInDb = _context.TypeProducts.Find(_typeProdcutKeybord.IdTypeProduct);
+
+        Assert.IsNotNull(editedTypeProductInDb);
+        Assert.AreEqual(_typeProdcutKeybord.TypeProductName, editedTypeProductInDb.TypeProductName);
+    }
+
+    [TestMethod]
+    public void ShouldNotUpdateBrandBecauseIdInUrlIsDifferent()
+    {
+        _typeProdcutScreen.TypeProductName = "OnlyFan";
+
+        // When
+        IActionResult action = _typeProductdController.Update(0, _typeProdcutScreen).GetAwaiter().GetResult();
+
+        // Then
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action, typeof(BadRequestResult));
+    }
+
+    [TestMethod]
+    public void ShouldNotUpdateBrandBecauseBrandDoesNotExist()
+    {
+        // When
+        IActionResult action = _typeProductdController.Update(_typeProdcutScreen.IdTypeProduct, _typeProdcutScreen).GetAwaiter().GetResult();
+
+        // Then
+        Assert.IsNotNull(action);
+        Assert.IsInstanceOfType(action, typeof(NotFoundResult));
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.TypeProducts.RemoveRange(_context.TypeProducts);
+        _context.SaveChanges();
     }
 }
